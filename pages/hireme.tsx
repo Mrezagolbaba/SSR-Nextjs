702d8f358a961c420c0fc6518192048af583feee
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-01-30 16:26:20 +0330

    create hire me page

---
+++ pages/hireme.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const Hireme=()=> {
+    return (
+      <Layout title="Hire Me">
+          <p>
+              you can hire me at{" "}
+              <a href="mailto:m.reza.golbaba@gmail.com">m.reza.golbaba@gmail.com</a>
+          </p>
+      </Layout>
+    )
+  }
+  export default Hireme;
